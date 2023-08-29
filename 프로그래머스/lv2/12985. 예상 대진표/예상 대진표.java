539b7f432817927dc11d9cac88a32b628f1a8f7d
이승열 <116939888+misracis2@users.noreply.github.com>
2023-08-29 22:33:34 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 84 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,19 @@
+class Solution
+{
+    public int solution(int n, int a, int b) {
+        int answer = 0;
+        while (a != b) {
+            if (a % 2 != 0) {
+                a++;
+            }
+            if (b % 2 != 0) {
+                b++;
+            }
+            a = a / 2;
+            b = b / 2;
+            answer++;
+        }
+
+        return answer;
+    }
+}
